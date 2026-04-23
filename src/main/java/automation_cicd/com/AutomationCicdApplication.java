e8018228059aff407d005c5ce4cd103658155e63
kanchon136 <miakanchon5100@gmail.ocm>
2026-04-23 13:19:10 +0600

    cidAutoRun

--- src/main/java/automation_cicd/com/AutomationCicdApplication.java
+++ src/main/java/automation_cicd/com/AutomationCicdApplication.java
@@ -10,6 +10,7 @@ public class AutomationCicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomationCicdApplication.class, args);
+		System.out.println("Automation Service is run..!!!");
 
 		//log.info("Automation Service is run..!!!");
 	}
